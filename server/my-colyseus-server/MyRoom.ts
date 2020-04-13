e6ac2bc3323860e908790b8f68055082bb89efa7
Jakub Záruba <info@jakub-zaruba.cz>
2020-04-13 19:18:15 +0200

    WIP - multiplayer

---
+++ server/my-colyseus-server/MyRoom.ts
@@ -0,0 +1,34 @@
+import { Room, Client } from "colyseus";
+import { Schema, MapSchema, type } from "@colyseus/schema";
+
+export class State extends Schema {
+
+  @type("number") foobar: number;
+
+}
+
+export class MyRoom extends Room {
+
+
+  onCreate(options: any) {
+    console.log(options);
+    // this.setState(new State());
+  }
+
+  onJoin(client: Client, options: any) {
+    console.log('client connected');
+  }
+
+  onMessage(client: Client, message: any) {
+    console.log('server message: ' + message);
+    this.broadcast(message);
+  }
+
+  onLeave(client: Client, consented: boolean) {
+    console.log('client disconnected');
+  }
+
+  onDispose() {
+  }
+
+}
